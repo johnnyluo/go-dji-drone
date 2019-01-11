e58cfda4a598d921390e03b6522071e0d870b31d
Johnny Luo <johnnyluo1980@gmail.com>
2019-01-11 14:50:52 +1100

    WIP

---
+++ cmd/video/go.mod
@@ -0,0 +1,8 @@
+module git.campmon.com/johnnyl/gobot/cmd/video
+
+require (
+	git.campmon.com/golang/corekit v1.7.0
+	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/veandco/go-sdl2 v0.3.0 // indirect
+	gobot.io/x/gobot v1.12.0
+)
---
+++ cmd/video/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+
+	"gobot.io/x/gobot"
+
+	"git.campmon.com/golang/corekit/proc"
+	"gobot.io/x/gobot/platforms/dji/tello"
+)
+
+var drone = tello.NewDriver("8888")
+
+const offset = 32767.0
+
+func main() {
+
+	mplayer := exec.Command("mplayer", "-fps", "60", "-")
+	mplayerIn, err := mplayer.StdinPipe()
+	if nil != err {
+		panic(err)
+	}
+
+	if err := mplayer.Start(); err != nil {
+		panic(err)
+	}
+
+	drone.On(tello.ConnectedEvent, func(data interface{}) {
+		drone.SetVideoEncoderRate(tello.VideoBitRateAuto)
+		drone.StartVideo()
+		gobot.Every(100*time.Millisecond, func() {
+			if err := drone.StartVideo(); nil != err {
+				fmt.Printf("fail to start video on drone:%s\n", err)
+			}
+		})
+	})
+
+	drone.On(tello.VideoFrameEvent, func(data interface{}) {
+		pkt := data.([]byte)
+		if len(pkt) > 0 {
+			if _, err := mplayerIn.Write(pkt); err != nil {
+				fmt.Printf("err:%s\n", err)
+			}
+		}
+	})
+
+	drone.On(tello.FlightDataEvent, func(data interface{}) {
+		fd := data.(*tello.FlightData)
+		printFlightData(fd)
+	})
+
+	if err := drone.Start(); nil != err {
+		fmt.Printf("drone err:%s \n", err)
+	}
+	donechan := make(chan struct{})
+	go droneCtrl(donechan)
+
+	if err := mplayer.Wait(); nil != err {
+		panic(err)
+	}
+	proc.WaitForTermination()
+	close(donechan)
+	// stop drone
+	drone.Halt()
+}
+func droneCtrl(done chan struct{}) {
+	fmt.Println("wait for input")
+	for {
+		select {
+		case <-done:
+			return
+		default:
+			reader := bufio.NewReader(os.Stdin)
+			item, err := reader.ReadString('\n')
+			if nil != err {
+				fmt.Printf("fail to read: %s", err)
+			}
+			fmt.Println(item)
+			switch item {
+			case "takeoff":
+				if err := drone.TakeOff(); nil != err {
+					fmt.Printf("fail to take off, err : %s", err)
+				}
+			case "w":
+				if err := drone.Up(5); nil != err {
+					fmt.Printf("drone fail to go up,err:%s", err)
+				}
+			case "a":
+				if err := drone.Left(5); nil != err {
+					fmt.Printf("drone fail to go left,err:%s", err)
+				}
+			case "d":
+				if err := drone.Right(5); nil != err {
+					fmt.Printf("drone fail to go right,err:%s", err)
+				}
+			case "s":
+				if err := drone.Down(5); nil != err {
+					fmt.Printf("drone fail to go left,err:%s", err)
+				}
+			case "land":
+				if err := drone.Land(); nil != err {
+					fmt.Printf("drone fail to land,err:%s", err)
+				}
+			}
+		}
+
+	}
+}
+
+func printFlightData(d *tello.FlightData) {
+	if d.BatteryLow {
+		fmt.Printf(" -- Battery low: %d%% --\n", d.BatteryPercentage)
+	}
+
+	// 	displayData := `
+	// Height:         %d
+	// Ground Speed:   %d
+	// Light Strength: %d
+	// `
+	//fmt.Printf(displayData, d.Height, d.GroundSpeed, d.LightStrength)
+}
